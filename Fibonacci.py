3c7b226ae97868897f948cf978dd86d9d31c7e88
ankita080208 <52229554+ankita080208@users.noreply.github.com>
2020-10-28 09:43:30 +0530

    Update Fibonacci.py
    did minor changes and added comments for explanation.

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,13 +1,16 @@
+#finding the fibonacci series using recursive method.
+#defining the function.
 def fibonacci(n):
-    if n == 1:
-        return 0
-
-    elif n ==2:
-        return 1
-
+#if the given input is less than or equal to 1 then return the following.
+    if n <= 1:
+        return n
+#if the given input is greater than 1 then return the following.
     else:
-        return fibonacci(n-1) + fibonacci(n-2) 
-
-
-number = int(input("Enter you number\n"))
-print(fibonacci(number))
+        return (fibonacci(n-1) + fibonacci(n-2)) 
+#storing the input as integer datatype given by user in a variable.
+n= int(input("Enter you number = "))
+#printing the result.
+print("fibonacci series is -")
+for i in range(n):
+   print(fibonacci(i)) 
+#end of code.
